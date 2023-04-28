aba2034c8fd5083bd512d3193201b88ba12f3a22
ariesbabu <66858372+ariesbabu@users.noreply.github.com>
2023-04-28 12:23:12 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ariesbabu/ocp-gitp/edit/main/',
+            'https://github.com/ariesbabu/rh-ocp/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
